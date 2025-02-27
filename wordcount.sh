0b538712f1c5bc120a368b1b5ed22b20d2c9d2f1
Jupiter <62066709+ItsJupiter21@users.noreply.github.com>
2025-02-27 23:18:21 +0300

    automate word count

--- wordcount.sh
+++ wordcount.sh
@@ -1,8 +1,7 @@
 
 # Get the word count number
-number=$(grep texcount EE.tex | grep 'Words in text' | grep -o '[0-9]*')
+number=$(texcount EE.tex | grep 'Words in text'| grep -o '[0-9]*')
 
 # Use sed to replace the line in EE.tex
-sed -i "s/          Word Count:.*/          Word Count:${number}/g" EE.tex
-
-echo "Word count updated in EE.tex to: ${number}
+sed -i "s/        Word Count:.*/        Word Count:${number}/g" EE.tex
+echo ${number}
